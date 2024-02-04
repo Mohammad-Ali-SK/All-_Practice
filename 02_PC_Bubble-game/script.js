4f91a41adbe0c0a3f3c5abdf55e898a6a5facc2f
Mohammad Ali sk <mohammadalisk050@gmail.com>
2024-02-04 10:49:40 +0530

    2/4/24

--- 02_PC_Bubble-game/script.js
+++ 02_PC_Bubble-game/script.js
@@ -1,58 +1,46 @@
-var randomNum = 0;
-var score = 0;
-function makeBubble(){
-    let culter = '';
-    for(let i = 0; i<=206; i++){
-        let ranNum = Math.floor(Math.random() * 10);
-        culter += `<div class="bubble">${ranNum}</div>`;
-    }
-    document.querySelector('.pan-btn').innerHTML = culter;  
+let score = 0;
+let hitNum = 0;
+function makeBuble(){
+    let culter = ' ';
+    for(let i = 0; i<=215; i++){
+        let randomNum = Math.floor(Math.random() * 10);
+        culter += `<div class="bubble">${randomNum}</div>`
+    };
+    document.querySelector('.pan-btn').innerHTML = culter;
 };
 
-
-function timeSet(){
-    let time = document.querySelector('.time').textContent;
-   let timer = setInterval((e) => {
+function time(){
+    let time = 9;
+    let timer = setInterval((e) => {
         if(time > 0){
             time--;
-            document.querySelector('.time').textContent = time;
+        document.querySelector('.time').textContent = time;
         }else{
             clearInterval(timer);
-            document.querySelector('.pan-btn').innerHTML = `<h1 class='over'>Game Over</h1>`;
-            document.querySelector('.hit').textContent = `Done`;
-            
-            // clearInterval(hit);
+            document.querySelector('.pan-btn').innerHTML =`<div>Your Scrore is ${score}</div>` ;
         }
     },1000)
 }
 
-function hit(){
-    randomNum = Math.floor(Math.random() * 10);
-    document.querySelector('.hit').textContent = randomNum;
+function ChHit(){
+    hitNum = Math.floor(Math.random() * 10);
+    document.querySelector('.hit').textContent = hitNum;
 }
-function scoreAdd(){
+function scoreP(){
     score += 10;
-    document.querySelector('.score').textContent = score;
+    document.querySelector('.scor').textContent = score;
 }
+let panBtn = document.querySelector('.pan-btn');
+panBtn.addEventListener('click',(e) => {
+   let bubble = Number(e.target.textContent);
+   if(bubble === hitNum){
+    scoreP()
+    ChHit();
+    makeBuble();
+   }
+})
 
-function selectBubble(){
-    document.querySelector('.pan-btn').addEventListener('click', (details) => {
-        let bubble = Number(details.target.textContent);
-        if(bubble === randomNum){
-            scoreAdd();
-            makeBubble();
-            hit();
-        }
-    })
-}
-
-
-
-
-makeBubble();
-timeSet();
-hit(); 
-selectBubble();
-
-
-
+time();
+makeBuble();
+ChHit();
+// scoreP();
